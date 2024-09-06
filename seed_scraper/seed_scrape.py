c47e513467797b62405578314eb8260123e76037
THoldon <HTHAM001@e.ntu.edu.sg>
2024-09-06 20:43:07 +0800

    added link from .sh to .py

--- seed_scraper/seed_scrape.py
+++ seed_scraper/seed_scrape.py
@@ -1,11 +1,29 @@
 import subprocess
 import time
+import sys
+import json
+import socket
 
+image_location = sys.argv[1]
+config_location = image_location[:-5] + "config.json"
 print("in python")
 
-image_location = "/home/ubuntu/FYP/images/rehosted_img_aug/87_813c23b285d234785f1c4ce19c76bf76d148e06977b6f016f6590e59d6d17f65.tar.gz" #image location, loop through all files
 #subprocess.Popen(['bash', 'docker_compose.sh',image_location]) #run script to docker compose with image location
-#time.sleep(20) #sleep to wait for docker compose to setup
-#print("after sleep")
+time.sleep(15) #sleep to wait for docker compose to setup
+print(image_location)
+print(config_location)
+with open(config_location, 'r') as config:
+    config_json = json.load(config)
+
+print(config_json)
+s = socket.socket()
+s.connect((config_json['targetip'],int(config_json['targetport'])))
+get_message = "GET / HTTP1.1\r\n\r\n"
+s.sendall(get_message.encode())
+val = s.recv(10000)
+
+print(val)
+
+print("after sleep")
 
 
--- seed_scraper/tar_loop.sh
+++ seed_scraper/tar_loop.sh
@@ -1,6 +1,6 @@
 script_location=$(pwd)
 
-cd /home/ubuntu/FYP/images/for_debug #probably create a folder to place images in and go there
+cd /home/ubuntu/FYP/images/for_debug #create a folder to place images in and go there
 
 for file in *.tar.gz; do
 	rm -r ../../extracted_image #create a folder to place extracted image
@@ -10,7 +10,7 @@ for file in *.tar.gz; do
 	cd ../../extracted_image/*/*/debug
 	img_location=$(pwd)
 	cd "$script_location"
-	./docker_compose.sh "$img_location"
+	./docker_compose.sh "$img_location" "$script_location"
 	echo "after docker_compose"
 	cd /home/ubuntu/FYP/images/for_debug
 done
