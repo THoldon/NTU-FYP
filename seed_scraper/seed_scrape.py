0e090014ae83db4779f13e0dce1ece157f438b80
THoldon <HTHAM001@e.ntu.edu.sg>
2024-10-03 18:17:19 +0800

    still working on extracting seed

--- seed_scraper/seed_scrape.py
+++ seed_scraper/seed_scrape.py
@@ -7,6 +7,8 @@ import os
 #import socket
 from Initializer import Initializer, WebCheck, Login
 from selenium import webdriver
+from scapy.all import *
+from scapy.layers.http import *
 
 def get_seed():
     os.chdir(sys.argv[2])
@@ -28,5 +30,32 @@ def get_seed():
     time.sleep(7)
     full_run = Initializer.full_run(brand,target,port,user,passwd,creds_dump_path)
 
+def extract_post():
+    http_pcap = PcapReader('seed.pcap')
+    found = 0
+    for pkt in http_pcap:
+        print(pkt.payload)
+        print("\n")
+        if HTTP in pkt:
+            if HTTPRequest in pkt:
+                        if pkt[HTTPRequest].Method == b'POST':
+                            print("\n\n")
+                            post_req = pkt[HTTPRequest]
+                            print(type(post_req))
+                            print(post_req)
+                            print("\n\n")
+                            #post_req = str(post_req)
+                            #post_req = post_req.decode("utf-8")
+                            #print(post_req)
+                            #print(type(post_req))
+                            payload = pkt.payload
+                            print(payload)
+                            print(type(payload))
+                            if(found == 0):
+                                found = 1
+                                continue
+            if(found == 1):
+                break
 
-get_seed()
+#get_seed()
+extract_post()
